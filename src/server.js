901a1101816e88cbfcc7b533e0b4489bb8539cf8
JadsDevolper <jdsnrodrigues@gmail.com>
2020-08-08 00:13:10 -0300

    script recebendo dados page-classes page-study

--- src/server.js
+++ src/server.js
@@ -36,17 +36,60 @@ const proffys = [
 
 ]
 
+const subjects = [
+
+  "Artes",
+  "Biologia",
+  "Ciências",
+  "Educação física",
+  "Física",
+  "Geografia",
+  "História",
+  "Matemática",
+  "Português",
+  "Química"
+]
+
+const weekdays = [
+  "Domingo",
+  "Segunda-feira",
+  "Terça-feira",
+  "Quarta-feira",
+  "Quinta-feira",
+  "Sexta-feira",
+  "Sábado"
+]
+
+function getSubject(subjectNumber) {
+  const position = +subjectNumber -1
+  return subjects[position]
+}
+
 function pageLanding(req, res) {
   return res.render("index.html")
 }
 
 function pageStudy(req, res) {
   const filters = req.query
-  return res.render("study.html", { proffys, filters })
+  return res.render("study.html", { proffys, filters, subjects, weekdays })
 }
 
 function pageGiveClasses(req, res) {
-  return res.render("give-classes.html")
+  const data = req.query
+
+  const isNoEmpty = Object.keys(data).length != 0
+
+  if(isNoEmpty){
+
+    data.subject = getSubject(data.subject)
+
+    proffys.push(data)
+
+    return res.redirect('/study')
+
+  }
+
+  return res.render("give-classes.html", {subjects, weekdays})
 }
 
 const express = require('express')
