c84abe764edaedff4f0da4473a3f3bf333d0ef3e
JadsDevolper <jdsnrodrigues@gmail.com>
2020-08-08 20:03:52 -0300

    aplicação pronta

--- src/server.js
+++ src/server.js
@@ -1,35 +1,6 @@
-const { subjects,  weekdays, getSubject } = require('./utils/format')
-
-function pageLanding(req, res) {
-  return res.render("index.html")
-}
-
-function pageStudy(req, res) {
-  const filters = req.query
-  return res.render("study.html", { proffys, filters, subjects, weekdays })
-}
-
-function pageGiveClasses(req, res) {
-  const data = req.query
-
-  const isNoEmpty = Object.keys(data).length != 0
-
-  if(isNoEmpty){
-
-    data.subject = getSubject(data.subject)
-
-    proffys.push(data)
-
-    return res.redirect('/study')
-
-  }
-
-  return res.render("give-classes.html", {subjects, weekdays})
-}
-
 const express = require('express')
 const server = express()
-
+const { pageLanding, pageStudy, pageGiveClasses, saveClasses } = require('./pages')
 const nunjucks = require('nunjucks')
 
 nunjucks.configure('src/views', {
@@ -37,10 +8,12 @@ nunjucks.configure('src/views', {
   noCache: true,
 })
 server
+.use(express.urlencoded({ extended: true }))
 .use(express.static('public'))
 
 .get('/', pageLanding)
 .get('/study', pageStudy)
 .get('/give-classes', pageGiveClasses)
+.post('/save-classes', saveClasses)
 
 .listen(5500)
